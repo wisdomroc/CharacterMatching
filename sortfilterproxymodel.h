fda40bb4c0bf88c1051a00c1a8cb8eb3a247cccd
wisdomroc <r.o.c.wang@163.com>
2019-09-24 16:42:32 +0800

    add My Custom QTableView & QAbstractTableModel to Ui

---
+++ sortfilterproxymodel.h
@@ -0,0 +1,11 @@
+#ifndef SORTFILTERPROXYMODEL_H
+#define SORTFILTERPROXYMODEL_H
+
+
+class SortFilterProxyModel : public QSortFilterProxyModel
+{
+public:
+    SortFilterProxyModel();
+};
+
+#endif // SORTFILTERPROXYMODEL_H
---
+++ tablemodel.h
@@ -0,0 +1,11 @@
+#ifndef TABLEMODEL_H
+#define TABLEMODEL_H
+
+
+class TableModel : public QAbstractTableModel
+{
+public:
+    TableModel();
+};
+
+#endif // TABLEMODEL_H
